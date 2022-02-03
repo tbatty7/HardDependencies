ea256530bc62827dfe28a62b6aa8ebd49748154f
Tim Batty <tbatty7@gmail.com>
2022-02-03 13:17:49 -0500

    Added 3 more XIB-based ViewControllers

---
+++ HardDependencies/InstanceInitializerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InstanceInitializerViewController.swift
+//  HardDependencies
+//
+//  Created by Timothy D Batty on 2/3/22.
+//
+
+import UIKit
+
+class InstanceInitializerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HardDependencies/MySingletonViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MySingletonViewController.swift
+//  HardDependencies
+//
+//  Created by Timothy D Batty on 2/3/22.
+//
+
+import UIKit
+
+class MySingletonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
